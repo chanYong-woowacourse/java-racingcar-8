8f3f7d1f36b05be162e430604b7b54320e1d4545
chanYong <yong113012@naver.com>
2025-10-25 23:56:37 +0900

    feat: Car 클래스 구현
    - 자동차 이름과 이동 거리를 관리하는 Car 클래스 생성
    - move() 메서드로 0~9 난수 생성 후 4 이상이면 전진 Randoms.pickNumberInRange()

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,32 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
-    String name;
+    private static final int MOVE_THRESHOLD = 4;
+    private static final int RANDOM_MIN = 0;
+    private static final int RANDOM_MAX = 9;
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move() {
+        int randomValue = Randoms.pickNumberInRange(RANDOM_MIN, RANDOM_MAX);
+        if (randomValue >= MOVE_THRESHOLD) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
